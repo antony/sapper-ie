c21833e98e46c42db0329791dd88b324ba81f8cb
Antony Jones <aj@desirableobjects.co.uk>
2019-06-29 22:39:57 +0100

    demonstrating CSS issue on page change

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,7 @@
-<svelte:head>
-	<title>About</title>
-</svelte:head>
+<style>
+	h1 {
+		color: red;
+	}
+</style>
 
-<h1>About this site</h1>
-
-<p>This is the 'about' page. There's not much here.</p>
+<h1>Also success!</h1>
